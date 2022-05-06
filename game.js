61ff7ad3d1585ca2cd71e50e00ae0ef91aca7306
Hajime <>
2022-05-06 13:44:01 +0800

    added reseting system, and some parts in jquery

--- game.js
+++ game.js
@@ -1,3 +1,23 @@
+// function
+function reset(){
+    daycount = 1;
+    $('.count-now').text(daycount);
+    hp = 100;
+    $('.hp-now').text(hp);
+    ep = 20;
+    $('.ep-now').text(ep);
+    sp = 0;
+    $('.sp-now').text(sp);
+    var item1num = 0;
+    $('.item-1-num').text(item1num);
+    var item2num = 0;
+    $('.item-2-num').text(item2num);
+    var item3num = 0;
+    $('.item-3-num').text(item3num);
+    $("#game").hide();
+    $("#start").show();
+}
+
 //start to mode//
 $(".btn-start").click(function(){
     $("#start").hide();
@@ -17,9 +37,9 @@ $(".mode-container-pred").click(function(){
 });
 //game to home//
 $(".action-quit").click(function(){
-    $("#game").hide();
-    $("#start").show();
+    reset();
 });
+
 //game action//
 $(".action-hunt").click(function(){
     $("#action").hide();
@@ -46,4 +66,49 @@ $(".action-skill").click(function(){
 $(".return-btn").click(function(){
     $("#skill").hide();
     $("#game").show();
+});
+
+//define
+//day count
+var daycount = 1;
+$('.count-now').text(daycount);
+//HP
+var hp = 100;
+$('.hp-now').text(hp);
+//EP
+var ep = 20;
+$('.ep-now').text(ep);
+//SP
+var sp = 15;
+$('.sp-now').text(sp);
+//meat num
+var item1num = 0;
+$('.item-1-num').text(item1num);
+//mushroom num
+var item2num = 0;
+$('.item-2-num').text(item2num);
+//water num
+var item3num = 0;
+$('.item-3-num').text(item3num);
+//action-end//
+
+$('.action-end').click(function(){
+    if(daycount >= 7){
+        reset();
+    }else{
+        hp = hp - 25;
+        $('.hp-now').text(hp);
+        if(hp <= 0){
+            $('.dialogue').html('<span>You have died<br/>Going back to start page</span>');
+            $('.dialogue').show();;
+            setTimeout(function(){
+                $('.dialogue').hide();
+                reset();
+           },1500);
+        }else{
+        ep = 20;
+        $('.ep-now').text(ep);
+        daycount = daycount + 1;
+        $('.count-now').text(daycount);}
+    };
 });
